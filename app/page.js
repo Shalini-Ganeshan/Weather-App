d44259b68f1da518a156f9332ae8a759cc6ede4d
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-07-04 13:40:06 +0530

    Updated searchBar

--- app/page.js
+++ app/page.js
@@ -78,6 +78,7 @@ setMyWeather(false);
     setResult(true);
     setLoading(true);
     setMyWeather(true);
+    setCity('');
     navigator.geolocation.getCurrentPosition((position) => {
       const { latitude, longitude } = position.coords;
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
